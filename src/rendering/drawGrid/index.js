719ea8b2d1dfe49363df7a6a23ca15c622de32a1
John Cole <johnwebbcole@gmail.com>
2018-09-16 13:00:18 -0500

    getting it working

--- src/rendering/drawGrid/index.js
+++ src/rendering/drawGrid/index.js
@@ -1,9 +1,9 @@
-const glslify = require('glslify')// -sync') // works in client & server
-const mat4 = require('gl-mat4')
-const path = require('path')
+const glslify = require("glslify"); // -sync') // works in client & server
+const mat4 = require("gl-mat4");
+const path = require("path");
 
-module.exports = function prepareDrawGrid (regl, params = {}) {
-  let positions = []
+module.exports = function prepareDrawGrid(regl, params = {}) {
+  let positions = [];
   const defaults = {
     color: [1, 1, 1, 1],
     ticks: 1,
@@ -11,83 +11,95 @@ module.exports = function prepareDrawGrid (regl, params = {}) {
     fadeOut: false,
     centered: false,
     lineWidth: 2
-  }
+  };
 
-  let {size, ticks, fadeOut, centered, lineWidth, color} = Object.assign({}, defaults, params)
+  let { size, ticks, fadeOut, centered, lineWidth, color } = Object.assign(
+    {},
+    defaults,
+    params
+  );
 
-  const width = size[0]
-  const length = size[1]
+  const width = size[0];
+  const length = size[1];
 
   if (false) {
-    const halfWidth = width * 0.5
-    const halfLength = length * 0.5
+    const halfWidth = width * 0.5;
+    const halfLength = length * 0.5;
     // const gridLine =
-    positions.push(-halfWidth, 0, 0)
-    positions.push(halfWidth, 0, 0)
+    positions.push(-halfWidth, 0, 0);
+    positions.push(halfWidth, 0, 0);
   }
 
   if (centered) {
-    const halfWidth = width * 0.5
-    const halfLength = length * 0.5
+    const halfWidth = width * 0.5;
+    const halfLength = length * 0.5;
 
-    const remWidth = halfWidth % ticks
-    const widthStart = -halfWidth + remWidth
-    const widthEnd = -widthStart
+    const remWidth = halfWidth % ticks;
+    const widthStart = -halfWidth + remWidth;
+    const widthEnd = -widthStart;
 
-    const remLength = halfLength % ticks
-    let lengthStart = -halfLength + remLength
-    const lengthEnd = -lengthStart
+    const remLength = halfLength % ticks;
+    let lengthStart = -halfLength + remLength;
+    const lengthEnd = -lengthStart;
 
-    const skipEvery = 0
+    const skipEvery = 0;
 
     for (let i = widthStart, j = 0; i <= widthEnd; i += ticks, j += 1) {
       if (j % skipEvery !== 0) {
-        positions.push(lengthStart, i, 0)
-        positions.push(lengthEnd, i, 0)
-        positions.push(lengthStart, i, 0)
+        positions.push(lengthStart, i, 0);
+        positions.push(lengthEnd, i, 0);
+        positions.push(lengthStart, i, 0);
       }
     }
     for (let i = lengthStart, j = 0; i <= lengthEnd; i += ticks, j += 1) {
       if (j % skipEvery !== 0) {
-        positions.push(i, widthStart, 0)
-        positions.push(i, widthEnd, 0)
-        positions.push(i, widthStart, 0)
+        positions.push(i, widthStart, 0);
+        positions.push(i, widthEnd, 0);
+        positions.push(i, widthStart, 0);
       }
     }
   } else {
     for (let i = -width * 0.5; i <= width * 0.5; i += ticks) {
-      positions.push(-length * 0.5, i, 0)
-      positions.push(length * 0.5, i, 0)
-      positions.push(-length * 0.5, i, 0)
+      positions.push(-length * 0.5, i, 0);
+      positions.push(length * 0.5, i, 0);
+      positions.push(-length * 0.5, i, 0);
     }
 
     for (let i = -length * 0.5; i <= length * 0.5; i += ticks) {
-      positions.push(i, -width * 0.5, 0)
-      positions.push(i, width * 0.5, 0)
-      positions.push(i, -width * 0.5, 0)
+      positions.push(i, -width * 0.5, 0);
+      positions.push(i, width * 0.5, 0);
+      positions.push(i, -width * 0.5, 0);
     }
   }
 
   return regl({
-    vert: glslify(path.join(__dirname, '/../basic.vert')),
-    frag: glslify(path.join(__dirname, '/shaders/grid.frag')),
+    vert: glslify(path.join(__dirname, "/../basic.vert")),
+    frag: glslify(path.join(__dirname, "/shaders/grid.frag")),
 
     attributes: {
       position: regl.buffer(positions)
     },
     count: positions.length / 3,
     uniforms: {
-      model: (context, props) => props && props.model ? props.model : mat4.identity([]),
-      color: (context, props) => props && props.color ? props.color : color,
-      fogColor: (context, props) => props && props.color ? [props.color[0], props.color[1], props.color[2], 0]
-        : [color[0], color[1], color[2], 0.0],
-      fadeOut: (context, props) => props && props.fadeOut !== undefined ? props.fadeOut : fadeOut
+      model: (context, props) =>
+        props && props.model ? props.model : mat4.identity([]),
+      color: (context, props) => (props && props.color ? props.color : color),
+      fogColor: (context, props) =>
+        props && props.color
+          ? [props.color[0], props.color[1], props.color[2], 0]
+          : [color[0], color[1], color[2], 0.0],
+      fadeOut: (context, props) =>
+        props && props.fadeOut !== undefined ? props.fadeOut : fadeOut
     },
-    lineWidth: (context, props) => Math.min((props && props.lineWidth ? props.lineWidth : lineWidth), regl.limits.lineWidthDims[1]),
-    primitive: 'lines',
+    lineWidth: (context, props) =>
+      Math.min(
+        props && props.lineWidth ? props.lineWidth : lineWidth,
+        regl.limits.lineWidthDims[1]
+      ),
+    primitive: "lines",
     cull: {
       enable: true,
-      face: 'front'
+      face: "front"
     },
     polygonOffset: {
       enable: true,
@@ -99,13 +111,12 @@ module.exports = function prepareDrawGrid (regl, params = {}) {
     blend: {
       enable: true,
       func: {
-        src: 'src alpha',
-        dst: 'one minus src alpha'
+        src: "src alpha",
+        dst: "one minus src alpha"
       }
     }
-
-  })
-}
+  });
+};
 
 /* alternate rendering method
 
